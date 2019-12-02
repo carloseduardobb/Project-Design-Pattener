9df57d35f686240d1e67164ad87ff1ce5e241af9
carloseduardobb <45433892+carloseduardobb@users.noreply.github.com>
2019-12-02 12:56:29 -0300

    Rename

--- ProjectDesignPattern/src/ProjectDesignPattern/Personagem.java
+++ ProjectDesignPattern/src/ProjectDesignPattern/Personagem.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * To change this license header, choose License Headers in Personagem Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -9,7 +9,7 @@ package ProjectDesignPattern;
  *
  * @author Carlos Eduardo
  */
-public class Project {
+public class Personagem {
 
     /**
      * @param args the command line arguments
