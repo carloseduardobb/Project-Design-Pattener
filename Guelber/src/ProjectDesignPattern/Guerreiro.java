6fb14e5a913d665cedcbd07b48533b87a618fb64
carloseduardobb <45433892+carloseduardobb@users.noreply.github.com>
2019-12-02 12:49:38 -0300

    Criando as classes

---
+++ Guelber/src/ProjectDesignPattern/Guerreiro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectDesignPattern;
+
+/**
+ *
+ * @author Carlos Eduardo
+ */
+public class Guerreiro implements RacaSelecionada{
+    
+    public void Classe(){
+        System.out.println("Classe selecionada: Guerreiro");
+    }
+}
